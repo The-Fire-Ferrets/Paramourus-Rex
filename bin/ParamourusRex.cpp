6dafbf4c8a52078e411128012281f4c7a6be4abe
William T Hollingsworth <wtholl@cs.wm.edu>
2015-10-01 17:26:12 -0400

    Remove unused SFML headers and libs.
    Also made more minor changes related to the change in our title.

--- bin/ParamourusRex.cpp
+++ bin/ParamourusRex.cpp
@@ -39,7 +39,7 @@ int main(int argc, char* argv[])
 			}
 		}
 	}
-	sf::RenderWindow App(sf::VideoMode(window_width, window_height,64), "The Last Dinosaur by Will Hollingsworth, Bina Kakusa, Belol Nessar, and Meghan Tinkler", sf::Style::Titlebar|sf::Style::Close);
+	sf::RenderWindow App(sf::VideoMode(window_width, window_height,64), "Paramourus Rex by Will Hollingsworth, Bina Kakusa, Belol Nessar, and Meghan Tinkler", sf::Style::Titlebar|sf::Style::Close);
 	
 	//Array holding levels in the order to be played
 	const char* levels[] = {"Level0"};
